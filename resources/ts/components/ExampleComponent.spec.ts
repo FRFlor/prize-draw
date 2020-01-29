72a2f800f84f16f729a89e832f73aa007011dcf0
Felipe Flor <f.flor@vehikl.com>
2020-01-29 18:26:52 -0500

    Make an example of useless component

--- resources/ts/components/ExampleComponent.spec.ts
+++
@@ -1,9 +0,0 @@
-import {mount} from '@vue/test-utils';
-import ExampleComponent from './ExampleComponent.vue';
-
-describe('ExampleComponent', () => {
-   it('Works', () => {
-      const wrapper = mount(ExampleComponent);
-      expect(wrapper.vm).toBeTruthy();
-   });
-});
