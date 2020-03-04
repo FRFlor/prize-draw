a0d52c1b29de644adf0c1d19631a8f714454e985
Felipe Flor <f.flor@vehikl.com>
2020-03-03 21:52:32 -0600

    Remove classes

--- resources/ts/components/RaffleBox.vue
+++ resources/ts/components/RaffleBox.vue
@@ -1,16 +1,12 @@
 <template>
     <div class="raffle-box">
-        <button :disabled="raffleInterval" class="start-raffling mb-4" @click="startRaffling">Start Raffling!</button>
+        <button :disabled="raffleInterval" class="start-raffling" @click="startRaffling">Start Raffling!</button>
 
-        <div class="row">
-            <div class="applicant-name col-sm-4 my-3"
+        <div class="">
+            <div class="applicant-name"
                  v-for="(applicant, index) in applicants"
                  :key="index">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">{{applicant}}</h5>
-                    </div>
-                </div>
+                    <h5 class="">{{applicant}}</h5>
             </div>
         </div>
 
@@ -53,25 +49,3 @@
         }
     }
 </script>
-
-<style lang="scss" scoped>
-    h5 {
-        margin: 0;
-    }
-
-    .winner-box {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: hsla(0, 0, 0, 0.5);
-        color: deepskyblue;
-        font-size: 5rem;
-        text-shadow: 2px 2px #1b1e21;
-        font-weight: bolder;
-    }
-</style>
