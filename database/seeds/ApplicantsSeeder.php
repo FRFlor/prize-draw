b2dc1a005721eccd8271a3b053334acc4293b230
Felipe Flor <f.flor@vehikl.com>
2020-03-25 19:38:48 -0400

    Update the applicants

--- database/seeds/ApplicantsSeeder.php
+++ database/seeds/ApplicantsSeeder.php
@@ -6,7 +6,7 @@ class ApplicantsSeeder extends Seeder
 {
     public function run()
     {
-        $allNames = ['Alex Jones-Chick', 'Lucy Kim', 'Alex Baletskyi', 'David Gleba', 'Chris Brown', 'Nuno Souto', 'Bob Bloom', 'Seied Mohammad Hosein Abdoli', 'Adam Brown', 'Jeonghwan Ju', 'Dave Redfern', 'Albert Ilyes', 'Reinhardt Christiansen'];
+        $allNames = ["Alex Jones-Chick", "Brian Chaves", "Lucy Kim", "Alex Baletskyi", "David Gleba", "Chris Brown", "Nuno Souto", "Bob Bloom", "Seied Mohammad Hosein Abdoli", "Adam Brown", "Jeonghwan Ju", "Dave Redfern", "Albert Ilyes", "Reinhardt Christiansen"];
 
         $applicantData = collect($allNames)->map(function($name) {
             return [
