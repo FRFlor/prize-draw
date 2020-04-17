6ac85b0533b12400f194811cb59ed90f59e86f27
Felipe Flor <f.flor@vehikl.com>
2020-04-17 09:07:26 -0400

    Implement endpoint that allows an applicant to be updated

--- app/Http/Controllers/ApplicantsController.php
+++ app/Http/Controllers/ApplicantsController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Applicant;
+use App\Http\Requests\UpdateApplicantRequest;
 use Illuminate\Http\Request;
 
 class ApplicantsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index');
+    }
+
     public function index()
     {
         return response()->json(Applicant::query()->select('id', 'name')->get());
+    }
+
+    public function update(UpdateApplicantRequest $request, Applicant $applicant)
+    {
+        $applicant->update($request->validated());
     }
 }
--- tests/Feature/ApplicantsControllerTest.php
+++ tests/Feature/ApplicantsControllerTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Applicant;
+use App\User;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class ApplicantsControllerTest extends TestCase
@@ -20,5 +22,41 @@ class ApplicantsControllerTest extends TestCase
         $this->getJson(route('applicants.index'))
             ->assertJsonCount($allApplicants->count())
             ->assertJson($applicantNames);
+    }
+
+    public function testAnAuthenticatedUserCanUpdateAGivenApplicantName()
+    {
+        $user = factory(User::class)->create();
+        $existingApplicant = factory(Applicant::class)->create();
+        $newName = 'Jack Bauer is definitely not the initial name';
+
+        $this
+            ->actingAs($user)
+            ->putJson(route('applicants.update', ['applicant' => $existingApplicant->id]), ['name' => $newName])
+            ->assertSuccessful();
+
+        $this->assertEquals($newName, $existingApplicant->fresh()->name, "The applicant's name was not updated");
+    }
+
+    public function testAGuestCannotUpdateAnApplicantsName()
+    {
+        $existingApplicant = factory(Applicant::class)->create();
+        $newName = 'Jack Bauer is definitely not the initial name';
+
+        $this
+            ->putJson(route('applicants.update', ['applicant' => $existingApplicant->id]), ['name' => $newName])
+            ->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function testAnApplicantsNameCannotBeSetToEmpty()
+    {
+        $user = factory(User::class)->create();
+        $existingApplicant = factory(Applicant::class)->create();
+        $newName = '';
+
+        $this
+            ->actingAs($user)
+            ->putJson(route('applicants.update', ['applicant' => $existingApplicant->id]), ['name' => $newName])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
